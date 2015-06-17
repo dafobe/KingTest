7a5f33b94dcb683ea4fb075bb10d613b18362bae
dfonollosa <dfonollosa@innofis.com>
2015-06-17 18:23:09 +0200

    merge

---
+++ KingTest/WebContent/js/directives/HeaderDrctv.js
@@ -0,0 +1,25 @@
+/**
+ * Directive: Inbox <inbox></inbox>
+ */
+angular.module('GamePortfolioApp')
+  .directive('portfolioheader', function HeaderDrctv () {
+    'use strict';
+
+    return {
+      restrict: 'EA',
+      replace: true,
+      scope: true,
+      templateUrl: "views/templates/header.tmpl.html",
+      controllerAs: 'header',
+
+      controller: function (NavigationFactory) {
+    	  this.goToGallery = function(){
+    		  NavigationFactory.renderGallery();
+    	  };
+    	  this.goToPortfolio = function(){
+    		  NavigationFactory.renderPortfolio();
+    	  };
+
+      }
+    }
+  });
